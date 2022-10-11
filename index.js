f706c5586f3acda82db0f363217f57f51abb3684
pedrofalco <fpfalco@gmail.com>
2022-10-11 16:49:26 -0300

    primera iteracion del minimo server viable para prototipar aplicaciones o testear sketches

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const server = app.listen(1111, () => console.log('listening @ 1111'));
+
+//EN CASO DE USARLO SIN FRONT, ELIMINAR LAS SIGUIENTES DOS LINEAS
+app.use(express.static('public'));
+app.use(express.json({
+    limit: '2mb'
+}));
