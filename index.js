7557cb035fbd19ee593d0076432ae4e578b3b381
pedrofalco <fpfalco@gmail.com>
2022-10-12 15:42:05 -0300

    pushing new repo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //UTILIZAR EL MODULO "EXPRESS"
 const express = require('express');
+const socket = require("socket.io");
 
+//EXPRESS
 const app = express();
 const server = app.listen(1111, () => console.log('listening @ 1111 --> http://localhost:1111'));
 
@@ -9,3 +11,19 @@ app.use(express.static('public'));
 app.use(express.json({
     limit: '2mb'
 }));
+
+
+//SOCKET.IO
+const io = socket(server);
+io.sockets.on('connection', newConnection);
+
+
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id);
+
+    socket.on('data', (data) => {
+        console.log(data);
+
+        io.emit('data', data);
+    });
+};
