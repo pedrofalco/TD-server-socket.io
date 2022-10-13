353b5298b1e4130a374e7ef91f77960ea6d793ee
pedrofalco <fpfalco@gmail.com>
2022-10-12 23:31:03 -0300

    modificando algunas cosas de la repo anterior

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const socket = require("socket.io");
 const app = express();
 const server = app.listen(1111, () => console.log('listening @ 1111 --> http://localhost:1111'));
 
-//EN CASO DE USARLO SIN FRONT, ELIMINAR LAS SIGUIENTES DOS LINEAS
-app.use(express.static('public'));
-app.use(express.json({
-    limit: '2mb'
-}));
-
-
 //SOCKET.IO
 const io = socket(server);
 io.sockets.on('connection', newConnection);
